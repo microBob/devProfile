806fd780860e9195f0eeca2a42594a57d00e3ce6
Microbob µ <microbob@is88.com>
2017-09-30 17:35:51 -0400

    init

---
+++ DevProfile/DevProfile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevProfile
+//
+//  Created by Kenneth on 9/30/17.
+//  Copyright © 2017 Microbob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
